14ca8c0df7c989ec1de2c0ca04c26a42f8b72e23
Tomasz Gola <tomasz@MacBook-Pro-Tomasz.local>
2018-08-21 09:05:23 +0200

    make tree of project, add two components and use react router to make links

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from '../Home/Home';
+import CommentsList from '../CommentsList/CommentsList';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Main component</p>
-      </div>
+      <Router>
+        <div>
+          <h2>My app</h2>
+          <Link to='/'>Home</Link>
+          <Link to='/comments' >Comments </Link>
+
+      
+          
+      
+          <Route exact path='/' component={Home}/>
+          <Route path='/comments' component={CommentsList}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/CommentsList/CommentsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CommentsList extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>List with comments</p>
+      </div>
+    );
+  }
+}
+
+export default CommentsList;
