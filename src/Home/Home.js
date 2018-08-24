c089c7c7a3d8d7881005d81a6f448a2c1ef1253f
Tomasz Gola <tomasz@MacBook-Pro-Tomasz.local>
2018-08-24 17:59:39 +0200

    add styling to make this app little prettier

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './Home.css'
 
 class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Hello at my app</p>
+      <div className="Home">
+        <h1>Hello at my app!</h1>
+        <img src='https://media.giphy.com/media/26xBwdIuRJiAIqHwA/source.gif'/>
       </div>
     );
   }
